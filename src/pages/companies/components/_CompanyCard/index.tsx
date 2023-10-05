e15a43af1b8d7e64b8c5b22e781bfe3b992e0058
Akif-Imran <raiden3322@gmail.com>
2023-10-05 16:35:46 +0500

    card view

---
+++ src/pages/companies/components/_CompanyCard/index.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import { useStyles } from "./styles";
+import { ActionIcon, Avatar, Card, Flex, Text, UnstyledButton, rem } from "@mantine/core";
+import { IconFiles, IconInfoSquareRounded, IconPlus } from "@tabler/icons-react";
+import { colors } from "@theme";
+
+interface OwnProps {
+  onClick: () => void;
+  item: ICompany;
+}
+
+const _CompanyCard: React.FC<OwnProps> = ({ onClick, item }) => {
+  const { cx, classes } = useStyles();
+  // const navigate = useNavigate();
+
+  // const handleNavigate = (route: string) => {
+  //   // toggleLayout();
+  //   navigate(route);
+  //   // setScrollIndex(index);
+  // };
+
+  const noImageStyle = {
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    image: {
+      filter: `invert(33%) sepia(65%) saturate(0%) hue-rotate(253deg) brightness(98%) contrast(88%)`,
+      objectFit: "contain",
+      width: rem(64),
+      height: rem(64),
+    },
+  };
+
+  return (
+    <Card shadow="sm" mb={"xs"} px={"sm"} py={"xs"} radius={"md"} onClick={onClick}>
+      <div className={classes.imageWithInfoContainer}>
+        <div className={classes.machineImageContainer}>
+          <Avatar
+            src={item?.logo ? `${item?.logo}` : "/car.png"}
+            radius={item?.logo ? rem(250) : rem(250)}
+            size={"xl"}
+            //@ts-expect-error style work
+            styles={item?.logo ? undefined : noImageStyle}
+          />
+        </div>
+        <div className={classes.infoContainer}>
+          <div className={classes.minorDetailsWithCount}>
+            <Flex direction={"row"} align={"center"} justify={"space-between"}>
+              <Text fw={"bold"} fs={"normal"} fz={"md"} color={colors.titleText}>
+                {item?.name}
+              </Text>
+              <ActionIcon
+                variant="transparent"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  // props?.toggleLayout();
+                  // setScrollIndex(index);
+                  // navigate(routes.vehicles.details_nav(item.id.toString()));
+                }}
+              >
+                <IconInfoSquareRounded size={22} stroke={1.3} color={colors.titleText} />
+              </ActionIcon>
+            </Flex>
+            <Text fw={"normal"} fs={"normal"} fz={"sm"} color={colors.titleText}>
+              {item?.email || "N/A"}
+            </Text>
+            <Text fw={"normal"} fs={"normal"} fz={"sm"} color={colors.titleText}>
+              {item?.phone || "N/A"}
+            </Text>
+            <Text fw={"normal"} fs={"normal"} fz={"sm"} color={colors.titleText}>
+              {item?.website || "N/A"}
+            </Text>
+            <Text fw={"normal"} fs={"normal"} fz={"sm"} color={colors.titleText}>
+              {item?.city || "N/A"}-{item?.state || "N/A"}-{item.country || "N/A"}
+            </Text>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.labelButtonsContainer}>
+        <UnstyledButton
+          className={cx(classes.bottomButton, classes.leftAlign)}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          <IconPlus stroke={1.3} size={22} color={colors.titleText} />
+          <Text fw={"normal"} fs={"normal"} fz={"sm"} ml={rem(4)} className={classes.labelButton}>
+            Contact
+          </Text>
+        </UnstyledButton>
+        <UnstyledButton
+          className={cx(classes.bottomButton, classes.leftAlign)}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          <IconPlus stroke={1.3} size={22} color={colors.titleText} />
+          <Text fw={"normal"} fs={"normal"} fz={"sm"} ml={rem(4)} className={classes.labelButton}>
+            Follow Up
+          </Text>
+        </UnstyledButton>
+        <UnstyledButton
+          className={cx(classes.bottomButton, classes.rightAlign)}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          <IconPlus stroke={1.3} size={22} color={colors.titleText} />
+          <Text
+            fw={"normal"}
+            fs={"normal"}
+            fz={"sm"}
+            ml={rem(4)}
+            mr={"xs"}
+            className={classes.labelButton}
+          >
+            Expense
+          </Text>
+        </UnstyledButton>
+        <UnstyledButton
+          className={cx(classes.bottomButton, classes.leftAlign)}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
+        >
+          <IconFiles stroke={1.3} size={22} color={colors.titleText} />
+          <Text fw={"normal"} fs={"normal"} fz={"sm"} ml={rem(4)} className={classes.labelButton}>
+            Reports
+          </Text>
+        </UnstyledButton>
+      </div>
+    </Card>
+  );
+};
+
+export { _CompanyCard };
---
+++ src/pages/companies/components/_CompanyCard/styles.ts
@@ -0,0 +1,63 @@
+import { createStyles, rem } from "@mantine/core";
+import { colors } from "@theme";
+
+export const useStyles = createStyles((theme) => ({
+  imageWithInfoContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  machineImageContainer: {
+    display: "flex",
+    paddingRight: rem(4),
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    // border: "1px solid black",
+  },
+  infoContainer: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    marginLeft: rem(4),
+    // border: "1px solid",
+  },
+  minorDetailsWithCount: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    // border: "1px solid black",
+  },
+  labelButtonsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  bottomButton: {
+    display: "flex",
+    flexDirection: "row",
+    // flex: 1,
+    paddingBlock: theme.spacing.xs,
+    paddingInline: theme.spacing.xs,
+    borderRadius: theme.radius.md,
+    justifyContent: "center",
+    alignItems: "center",
+    "&:hover": {
+      backgroundColor: theme.fn.lighten(colors.mediumGray, 0.6),
+    },
+  },
+  leftAlign: {
+    justifyContent: "flex-start",
+  },
+  rightAlign: {
+    justifyContent: "flex-end",
+  },
+  labelButton: {
+    color: colors.titleText,
+    // textDecorationLine: "underline",
+    accentColor: colors.titleText,
+  },
+}));
